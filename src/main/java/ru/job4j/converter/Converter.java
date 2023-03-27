6865997712e4eece67bea05e987a792f197504d0
Sergey Krayushkin <Krayushkin.sergey@gmail.com>
2023-03-27 19:06:52 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,13 +2,11 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60;
-        return rsl;
+        return value / 60;
     }
 
     public static void main(String[] args) {
@@ -21,6 +19,7 @@ public class Converter {
         in = 150;
         expected = 2.5f;
         out = Converter.rubleToDollar(in);
+        passed = expected == out;
         System.out.println("150 rubles are 2.5 dollar. Test result : " + passed);
     }
 }
