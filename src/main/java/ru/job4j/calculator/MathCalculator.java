74abb30e870296fce14cd068fdb0751d2be2e38f
Sergey Krayushkin <Krayushkin.sergey@gmail.com>
2023-03-27 20:13:15 +0300

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second) + multiply(first, second);
+    }
+
+    public static double sumDiffDiv(double first, double second) {
+        return difference(first, second) + division(first, second);
+    }
+
+    public static double sumAllOper(double first, double second) {
+        return sumAndMultiply(first, second) + sumDiffDiv(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+
+        System.out.println("Результат расчета всех операций равен: " + sumAllOper(20, 10));
+    }
+}
