b124e99999e68c193ce4b2ba3112aa7aa8606dbd
Sergey Krayushkin <Krayushkin.sergey@gmail.com>
2023-03-28 07:14:00 +0300

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -5,22 +5,17 @@ import org.junit.Assert;
 public class X2Test {
     @Test
     public void whenA10B0C0X2Then40() {
-        /* Входные параметра. Их будет 4 для данного случая*/
         int a = 10;
         int b = 0;
         int c = 0;
         int x = 2;
-        /* ожидаемое значение. Это всегда одна переменная. */
         int expected = 40;
-        /* вызов метода, который хотим проверить. */
-        /* Результат вычисления. Это всегда одна переменная. */
         int rsl = X2.calc(a, b, c, x);
-        /* сравнение полученного значения с ожидаемым */
         Assert.assertEquals(expected, rsl);
     }
 
     @Test
-    public void whenA1B1C1X1Then40() {
+    public void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
@@ -31,7 +26,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA0B1C1X1Then40() {
+    public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
@@ -42,7 +37,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C0X1Then40() {
+    public void whenA1B1C0X1Then2() {
         int a = 1;
         int b = 1;
         int c = 0;
@@ -53,7 +48,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA1B1C1X0Then40() {
+    public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
         int c = 1;
