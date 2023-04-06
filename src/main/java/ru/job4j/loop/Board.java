571f228d820adfb38ae284f7c635a01a2026fe15
Sergey Krayushkin <Krayushkin.sergey@gmail.com>
2023-04-06 21:37:46 +0300

    5.3. Построить шахматную доску в псевдографике. [#13559]

--- src/main/java/ru/job4j/loop/Board.java
+++ src/main/java/ru/job4j/loop/Board.java
@@ -4,8 +4,6 @@ public class Board {
     public static void paint(int width, int height) {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                /* условие проверки, что писать пробел или X */
-                /* Выше в задании мы определили закономерность, когда нужно проставлять X */
                 if ((i + j) % 2 == 0) {
                     System.out.print("X");
                 } else {
